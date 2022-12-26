17e9d7f7216a28ab2b60dff2726048240c727c35
limaizabela <contatoizabela@outlook.com>
2022-12-25 22:39:11 -0300

    desafio-media-idade-da-turma

---
+++ turno/README.md
@@ -0,0 +1,8 @@
+## Desafio
+Programa que pergunta em que turno você estuda, pede para digitar M-matutino ou V-Vespertino ou N- Noturno e imprime a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+
+## Entrada
+A entrada consiste em um caractere(char) M , N ou V que informará o turno em que você estuda.
+
+## Saída
+A saída mostrará uma mensagem que informa conforme o seu turno.
---
+++ turno/src/Turno.java
@@ -0,0 +1,28 @@
+// IMPORTANTE: Para ler os dados de entrada do usuário, utilize a classe Scanner, 
+// instanciando seu leitor da seguinte forma: "Scanner leitor = new Scanner(System.in);". 
+// Por outro lado, para imprimir suas saídas, utilize System.out.print ou System.out.println.
+
+import java.security.Signature;
+import java.util.Scanner;
+
+public class Turno {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Informe em qual turno você estuda (M/T/N): ");
+        char turno = leitor.next().toUpperCase().charAt(0);
+
+
+            if (turno == 'm' || turno == 'M') {
+                System.out.println("Bom Dia!");
+            }else if (turno == 'v' || turno == 'V') { 
+                System.out.println("Boa tarde!");
+            }else if (turno == 'n' || turno == 'N') { 
+                System.out.println("Boa Noite!");
+            }else{
+                System.out.println("Valor Inválido!");
+            }
+              
+    
+        }
+
+}
