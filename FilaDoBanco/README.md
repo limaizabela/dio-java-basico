4142a131d1b7724b260699e1c41d9bd8ce5b42ce
limaizabela <contatoizabela@outlook.com>
2022-12-27 00:05:12 -0300

    Desafio Fila do Banco

---
+++ FilaDoBanco/README.md
@@ -0,0 +1,8 @@
+## Desafio
+Este programa simula uma fila de atendimento bancário. O programa ler o nome de 3 pessoas (clientes do banco), armazenando-os em uma fila.
+
+## Entrada
+A entrada será o nome de três pessoas (clientes do banco).
+
+## Saída
+O programa deve imprimir como saída na tela o nome da pessoa e sua posição na fila.
---
+++ FilaDoBanco/src/FilaDoBanco.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FilaDoBanco {
+ 
+    public static void main(String[] args) { 
+    
+    String nomesFila[] = new String[3];
+    Scanner nome = new Scanner(System.in);
+     
+    int i;
+
+    for (i = 0; i < nomesFila.length; i++) {
+        System.out.println("Informe o nome: ");
+        nomesFila[i] = nome.next();
+
+    }
+
+
+    System.out.println(nomesFila[0]+ " - está na posição: 1");
+    System.out.println(nomesFila[1]+ " - está na posição: 2");
+    System.out.println(nomesFila[2]+ " - está na posição: 3");
+      
+    }
+
+}
