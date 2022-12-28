5a6d83575a9956b229aa885b0f2b69ba662c8272
limaizabela <contatoizabela@outlook.com>
2022-12-28 15:29:18 -0300

    Mínimo múltiplo

---
+++ MinimoMultiplo/README.md
@@ -0,0 +1,8 @@
+## Descrição
+Dado um inteiro positivo n, retorne o menor inteiro positivo que é múltiplo de 2 e de n.
+
+## Entrada
+A entrada consiste em um int, do qual podemos calcular seu menor multiplo.
+
+## Saída
+A saída consiste em um retorno, que será o menor multiplo do int digitado e de 2.
---
+++ MinimoMultiplo/src/MinimoMultiplo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MinimoMultiplo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe um número inteiro: ");
+        int numero = scanner.nextInt();
+        
+//TODO: Crie as outras condições necessárias para a resolução do desafio.
+        int minimoMultiplo = 0;
+        if (numero % 2 == 0) {
+            minimoMultiplo = numero;
+        } else {
+            minimoMultiplo = numero * 2;
+        }
+        
+        System.out.println("O resultado é:c" + minimoMultiplo);
+        }
+}
